0f413de8f7d8024c80c01991e0c46d387b815673
Dhruv Sharma <yesdhruvsharma@gmail.com>
2026-07-19 01:48:35 +0530

    Repository Unit Testing

---
+++ SmartLedgerAI.Tests/Repository/ExpenseRepositoryTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLedgerAI.Tests.Repository
+{
+    internal class ExpenseRepositoryTests
+    {
+    }
+}

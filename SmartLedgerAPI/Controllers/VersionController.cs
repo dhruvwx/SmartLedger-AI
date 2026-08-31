4b4ba5db95dda456aa1db24b1e0d18331d5a0a45
Dhruv Sharma <yesdhruvsharma@gmail.com>
2026-09-01 00:11:19 +0530

    Add Version Controller to differentiate between slots (staging/production).

---
+++ SmartLedgerAPI/Controllers/VersionController.cs
@@ -0,0 +1,22 @@
+﻿ using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartLedgerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var versionObject = new
+            {
+                version = typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
+                environments = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production",
+                deployedAt = System.IO.File.GetCreationTimeUtc(typeof(Program).Assembly.Location)
+                            };
+            return Ok(versionObject);
+        }
+    }
+}
